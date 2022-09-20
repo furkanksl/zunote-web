b81097ceaaa9dacdac714771dbdd0d137efaf71a
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-09-20 14:50:52 +0300

    Update header svgs with svg components

--- src/services/local.service.ts
+++ src/services/local.service.ts
@@ -2,18 +2,4 @@ export default class LocalService {
     constructor() {}
 
     initialize() {}
-
-    clickOutsideHandlerOnMobileMenu(e: any): boolean {
-        if (e !== undefined) {
-            const mobileMenuEl = !document?.getElementById("mobile-menu")?.contains(e.target);
-
-            if (mobileMenuEl ?? false) {
-                const closeMenuButtonEl = !document.getElementById("close-mobile-menu-button")?.contains(e.target);
-                if (closeMenuButtonEl ?? false) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 }
