b650665e9336131f92b62a7b49bb79450ff27846
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-09-25 00:49:31 +0300

    Change category tab bg color

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const HomePage = dynamic(() => import("../src/pages/home/HomePage"), { ssr: false });
+const HomePage = dynamic(async () => await import("../src/pages/home/HomePage"), { ssr: false });
 
 function Home() {
     return <HomePage />;
