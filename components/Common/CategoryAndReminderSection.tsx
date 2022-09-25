00ab5edcbe762fad33a957bccfbdea9507e6f797
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-09-25 16:16:16 +0300

    Add select category functionality in category dialog

--- components/Common/CategoryAndReminderSection.tsx
+++ components/Common/CategoryAndReminderSection.tsx
@@ -8,26 +8,37 @@ import DatetimePicker from "../DatetimePicker";
 
 import styles from "./Common.module.scss";
 import moment from "moment";
+import { setIsCategoryVisible } from "../../src/redux/features/dialog.reducer";
+import { setSelectedCategory } from "../../src/redux/features/category.reducer";
 
 function CategoryAndReminderSection() {
     const dispatch = useDispatch();
 
     const isReminderSelected = useSelector((state: any) => state.reminder.value);
     const reminderDate = useSelector((state: any) => state.reminder.reminderDate);
+    const selectedCategory = useSelector((state: any) => state.category.selectedCategory);
     var dateString = moment.utc(reminderDate).format("YYYY-MMM-DD HH:mm");
 
     function removeReminder() {
         dispatch(unsetReminder());
     }
 
+    function openCategoryDialog() {
+        dispatch(setIsCategoryVisible(true));
+    }
+
+    function removeSelectedCategory() {
+        dispatch(setSelectedCategory(""));
+    }
+
     return (
         <div className={styles["reminder-and-category-container"]}>
             <div className={styles["category-row"]}>
                 <div className={styles["icon-and-text"]}>
-                    <CategorySvgComponent function={() => {}} />
-                    <p>Category 1</p>
+                    <CategorySvgComponent function={openCategoryDialog} />
+                    <p onClick={openCategoryDialog}>{selectedCategory === "" ? "Set category" : selectedCategory}</p>
                 </div>
-                <RemoveSvgComponent function={() => {}} />
+                <RemoveSvgComponent function={removeSelectedCategory} />
             </div>
             <div className={styles["reminder-row"]}>
                 <div className={styles["icon-and-text"]}>
