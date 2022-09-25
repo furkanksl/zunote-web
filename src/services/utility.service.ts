d9762be952d770b426485f7f93de6a7d30b61bd2
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-09-25 15:44:57 +0300

    Update utility service with utc to local time functioanlity

--- src/services/utility.service.ts
+++ src/services/utility.service.ts
@@ -1,5 +1,12 @@
+import moment from "moment";
+
 export default class UtilityService {
     formatRecorderTime(value: number): string {
         return value < 10 ? `0${value}` : `${value}`;
+    }
+
+    timestampToString(value: number): string {
+        const localTime = moment.utc(value).toDate();
+        return moment(localTime).format("DD-MMM-YYYY  HH:mm");
     }
 }
