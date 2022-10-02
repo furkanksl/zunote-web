267c2c81f5d6e14eedc0998673d5d7e8a36e6691
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-03 02:02:38 +0300

    Fix recorded audio data issue on save

--- src/pages/home/components/Recorder.tsx
+++ src/pages/home/components/Recorder.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useRecorder } from "voice-recorder-react";
+import { AudioData, useRecorder } from "voice-recorder-react";
 
 import MicSvgComponent from "../../../../components/Svg/MicSvg";
 import PauseSvgComponent from "../../../../components/Svg/PauseSvg";
@@ -10,7 +10,7 @@ import { setIsRecording, setLapTime } from "../../../redux/features/recorder.red
 import UtilityService from "../../../services/utility.service";
 
 type Props = {
-    onRecord: () => any;
+    onRecord: (data: AudioData) => any;
 };
 
 function Recorder(props: Props) {
@@ -20,56 +20,48 @@ function Recorder(props: Props) {
     const [isActive, setIsActive] = useState(false);
     const [isStarted, setIsStarted] = useState(false);
 
-    const audioRef = useRef<HTMLAudioElement>(null);
-    const { time, data, stop, start, pause, paused, resume, recording } = useRecorder();
+    const { time, data, stop, start, pause, resume } = useRecorder();
 
     useEffect(() => {
-        if (data.url && audioRef.current) {
-            audioRef.current.src = data.url;
+        if (data.blob.size !== 0) {
+            props.onRecord(data);
         }
+    }, [data?.url]);
+
+    useEffect(() => {
         dispatch(
             setLapTime(utilityService.formatRecorderTime(time?.m) + ":" + utilityService.formatRecorderTime(time?.s))
         );
-    }, [data?.url, time]);
+    }, [time]);
 
     function startVoiceRecording() {
         start();
+
         setIsActive(true);
         setIsStarted(true);
+
         dispatch(setIsRecording(true));
     }
 
     function pauseVoiceRecording() {
         pause();
+
         setIsActive(false);
     }
 
     function resumeVoiceRecording() {
         resume();
+
         setIsActive(true);
     }
 
     function stopVoiceRecording() {
-        props.onRecord();
         stop();
         setIsActive(false);
         setIsStarted(false);
+
         dispatch(setIsRecording(false));
     }
-
-    // eslint-disable-next-line
-    const convertFileToBase64 = (file: any) =>
-        new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file.mediaBlobUrl);
-
-            reader.onload = () =>
-                resolve({
-                    fileName: file.title,
-                    base64: reader.result,
-                });
-            reader.onerror = reject;
-        });
 
     return (
         <>
