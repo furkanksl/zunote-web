a77a79e6d375a849c8dedb3c05b3593a65f8dc3b
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-04 01:51:15 +0300

    Add file names to get-voices api as a return

--- pages/api/get-voices.ts
+++ pages/api/get-voices.ts
@@ -18,10 +18,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
             });
 
             const resp = await signedUrlResponse.json();
+            const voiceUrls = await getVoiceRecordUrlsByName(resp.files, req.body["uuid"]);
 
             res.status(200).send({
                 status: resp.status,
-                fileNames: resp.files,
+                voiceUrls: voiceUrls,
             });
         } catch (error) {
             res.status(409).send({
@@ -32,4 +33,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         res.status(400).send({
             status: "Bad Request!",
         });
+}
+
+async function getVoiceRecordUrlsByName(fileNames: any[], uuid: string): Promise<string[]> {
+    const GET_URL_BY_FILE_NAME_API = process.env.API_GET_URL_BY_FILE_NAME!;
+
+    const voiceUrls: any[] = [];
+
+    for (let index = 0; index < fileNames.length; index++) {
+        try {
+            const response = await fetch(GET_URL_BY_FILE_NAME_API, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    uuid: uuid,
+                    fileName: fileNames[index],
+                    fileType: "audio/mpeg",
+                }),
+                cache: "default",
+            });
+            const fileUrl = (await response.json()).url;
+
+            if (response.url) voiceUrls.push({ url: fileUrl, fileName: fileNames[index] });
+        } catch (error) {}
+    }
+
+    return voiceUrls;
 }
