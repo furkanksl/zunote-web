9e70588c3fb933a1b1e0584b228aed88df3eb88c
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-04 16:55:50 +0300

    Update note reducer

--- src/redux/features/note.reducer.ts
+++ src/redux/features/note.reducer.ts
@@ -2,162 +2,162 @@ import { createSlice } from "@reduxjs/toolkit";
 import Note from "../../models/Note.model";
 import VoiceNote from "../../models/VoiceNote.model";
 
-//! DUMMY NOTE DATAS
-const textNotes: Note[] = [
-    new Note({
-        category: "Cat1",
-        createdAt: 0,
-        noteText:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+// //! DUMMY NOTE DATAS
+// const textNotes: Note[] = [
+//     new Note({
+//         category: "Cat1",
+//         createdAt: 0,
+//         noteText:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
 
-        reminder: 0,
-    }),
-    new Note({
-        category: "Cat2",
-        createdAt: 0,
-        noteText:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//         reminder: 0,
+//     }),
+//     new Note({
+//         category: "Cat2",
+//         createdAt: 0,
+//         noteText:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
 
-        reminder: 0,
-    }),
-    new Note({
-        category: "Cat3",
-        createdAt: 0,
-        noteText:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-        reminder: 0,
-    }),
-    new Note({
-        category: "Cat4",
-        createdAt: 0,
-        noteText:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-        reminder: 0,
-    }),
-    new Note({
-        category: "Cat5",
-        createdAt: 0,
-        noteText:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-        reminder: 0,
-    }),
-    new Note({
-        category: "Cat6",
-        createdAt: 0,
-        noteText:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-        reminder: 0,
-    }),
-];
+//         reminder: 0,
+//     }),
+//     new Note({
+//         category: "Cat3",
+//         createdAt: 0,
+//         noteText:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//         reminder: 0,
+//     }),
+//     new Note({
+//         category: "Cat4",
+//         createdAt: 0,
+//         noteText:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//         reminder: 0,
+//     }),
+//     new Note({
+//         category: "Cat5",
+//         createdAt: 0,
+//         noteText:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//         reminder: 0,
+//     }),
+//     new Note({
+//         category: "Cat6",
+//         createdAt: 0,
+//         noteText:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//         reminder: 0,
+//     }),
+// ];
 
-const voiceNotes: VoiceNote[] = [
-    new VoiceNote({
-        category: "Cat1",
-        createdAt: 0,
-        reminder: 0,
-        voiceUrl: "http",
-        notes: [
-            {
-                createdAt: 0,
-                noteText:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-                time: "00:10",
-            },
-            {
-                createdAt: 0,
-                noteText:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-                time: "01:10",
-            },
-            {
-                createdAt: 0,
-                noteText:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-                time: "01:14",
-            },
-            {
-                createdAt: 0,
-                noteText:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-                time: "01:45",
-            },
-            {
-                createdAt: 0,
-                noteText:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-                time: "01:45",
-            },
-            {
-                createdAt: 0,
-                noteText:
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
-                time: "01:45",
-            },
-        ],
-    }),
-    new VoiceNote({
-        category: "Cat2",
-        createdAt: 0,
-        reminder: 0,
-        voiceUrl: "http",
-        notes: [
-            { createdAt: 0, noteText: "adasdasd", time: "00:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:14" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:45" },
-        ],
-    }),
-    new VoiceNote({
-        category: "Cat3",
-        createdAt: 0,
-        reminder: 0,
-        voiceUrl: "http",
-        notes: [
-            { createdAt: 0, noteText: "adasdasd", time: "00:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:14" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:45" },
-        ],
-    }),
-    new VoiceNote({
-        category: "Cat4",
-        createdAt: 0,
-        reminder: 0,
-        voiceUrl: "http",
-        notes: [
-            { createdAt: 0, noteText: "adasdasd", time: "00:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:14" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:45" },
-        ],
-    }),
-    new VoiceNote({
-        category: "Cat5",
-        createdAt: 0,
-        reminder: 0,
-        voiceUrl: "http",
-        notes: [
-            { createdAt: 0, noteText: "adasdasd", time: "00:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:14" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:45" },
-        ],
-    }),
-    new VoiceNote({
-        category: "Cat6",
-        createdAt: 0,
-        reminder: 0,
-        voiceUrl: "http",
-        notes: [
-            { createdAt: 0, noteText: "adasdasd", time: "00:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:10" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:14" },
-            { createdAt: 0, noteText: "adasdasd", time: "01:45" },
-        ],
-    }),
-];
+// const voiceNotes: VoiceNote[] = [
+//     new VoiceNote({
+//         category: "Cat1",
+//         createdAt: 0,
+//         reminder: 0,
+//         voiceUrl: "http",
+//         notes: [
+//             {
+//                 createdAt: 0,
+//                 noteText:
+//                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//                 time: "00:10",
+//             },
+//             {
+//                 createdAt: 0,
+//                 noteText:
+//                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//                 time: "01:10",
+//             },
+//             {
+//                 createdAt: 0,
+//                 noteText:
+//                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//                 time: "01:14",
+//             },
+//             {
+//                 createdAt: 0,
+//                 noteText:
+//                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//                 time: "01:45",
+//             },
+//             {
+//                 createdAt: 0,
+//                 noteText:
+//                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//                 time: "01:45",
+//             },
+//             {
+//                 createdAt: 0,
+//                 noteText:
+//                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum fugiat quibusdam sit quidem a deleniti quam culpa beatae corrupti, reiciendis consequatur, esse distinctio eius, mollitia veniam ullam doloremque at!",
+//                 time: "01:45",
+//             },
+//         ],
+//     }),
+//     new VoiceNote({
+//         category: "Cat2",
+//         createdAt: 0,
+//         reminder: 0,
+//         voiceUrl: "http",
+//         notes: [
+//             { createdAt: 0, noteText: "adasdasd", time: "00:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:14" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:45" },
+//         ],
+//     }),
+//     new VoiceNote({
+//         category: "Cat3",
+//         createdAt: 0,
+//         reminder: 0,
+//         voiceUrl: "http",
+//         notes: [
+//             { createdAt: 0, noteText: "adasdasd", time: "00:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:14" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:45" },
+//         ],
+//     }),
+//     new VoiceNote({
+//         category: "Cat4",
+//         createdAt: 0,
+//         reminder: 0,
+//         voiceUrl: "http",
+//         notes: [
+//             { createdAt: 0, noteText: "adasdasd", time: "00:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:14" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:45" },
+//         ],
+//     }),
+//     new VoiceNote({
+//         category: "Cat5",
+//         createdAt: 0,
+//         reminder: 0,
+//         voiceUrl: "http",
+//         notes: [
+//             { createdAt: 0, noteText: "adasdasd", time: "00:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:14" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:45" },
+//         ],
+//     }),
+//     new VoiceNote({
+//         category: "Cat6",
+//         createdAt: 0,
+//         reminder: 0,
+//         voiceUrl: "http",
+//         notes: [
+//             { createdAt: 0, noteText: "adasdasd", time: "00:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:10" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:14" },
+//             { createdAt: 0, noteText: "adasdasd", time: "01:45" },
+//         ],
+//     }),
+// ];
 
-const allNotes = [...textNotes, ...voiceNotes];
+const allNotes: any = []; // [...textNotes, ...voiceNotes];
 
 type NoteState = {
     notes: any[];
@@ -179,6 +179,10 @@ export const noteSlice = createSlice({
     },
 
     reducers: {
+        setNotes: (state: NoteState, action: any) => {
+            state.notes = [...action.payload];
+        },
+
         addNewNote: (state: NoteState, action: any) => {
             state.notes.splice(0, 0, action.payload);
         },
@@ -219,6 +223,7 @@ export const {
     setSelectedNoteIndex,
     setIsNoteEditing,
     setNotesTabIndex,
+    setNotes,
 } = noteSlice.actions;
 
 export default noteSlice.reducer;
