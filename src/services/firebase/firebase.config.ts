ab762a8a86168939c923cb0dc13a1b4e1793c96a
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-04 16:56:57 +0300

    Update firebase config with db url

--- src/services/firebase/firebase.config.ts
+++ src/services/firebase/firebase.config.ts
@@ -7,4 +7,5 @@ export const firebaseConfig = {
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
 };
