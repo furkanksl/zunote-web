483447db9a1956d3ac117d47baed3c49601d2dc8
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-04 17:08:00 +0300

    Update notes page

--- src/pages/notes/NotesPage.tsx
+++ src/pages/notes/NotesPage.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setNotes } from "../../redux/features/note.reducer";
+import { StateModel } from "../../redux/store/store";
+import AwsService from "../../services/aws.service";
+import FirebaseService from "../../services/firebase/firebase.service";
+import UtilityService from "../../services/utility.service";
 import CategoryTabs from "./components/CategoryTabs";
 import NoteList from "./components/NoteList";
 import SelectCategoryBox from "./components/SelectCategoryBox";
@@ -5,6 +12,25 @@ import SelectCategoryBox from "./components/SelectCategoryBox";
 import styles from "./NotesPage.module.scss";
 
 function NotesPage() {
+    const dispatch = useDispatch();
+
+    const allNotes = useSelector((state: StateModel) => state.note.notes);
+
+    const firebaseService = new FirebaseService();
+    const awsService = new AwsService();
+    const utilityService = new UtilityService();
+
+    useEffect(() => {
+        if (allNotes.length === 0) {
+            new Promise(async (resolve, reject) => {
+                let notes = await firebaseService.getAllNotes();
+                const voiceNotesUrls = await awsService.getVoiceRecords();
+                notes = utilityService.appendVoiceUrlsToVoiceNotes(notes, voiceNotesUrls);
+                dispatch(setNotes(notes));
+            });
+        }
+    }, []);
+
     return (
         <div className={styles["notes-page-wrapper"]}>
             <CategoryTabs />
