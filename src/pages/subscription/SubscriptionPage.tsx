d1fbbb1010639c6522e0eea3de8b86f6e6b851d5
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-07 20:14:49 +0300

    Update subs page

--- src/pages/subscription/SubscriptionPage.tsx
+++ src/pages/subscription/SubscriptionPage.tsx
@@ -1,13 +1,38 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../../redux/features/user.reducer";
+import { StateModel } from "../../redux/store/store";
+
+import LoadingSvgComponent from "../../../components/Svg/LoadingSvg";
+import UserData from "../../models/UserData.model";
+import FirebaseService from "../../services/firebase/firebase.service";
 import CurrentPlan from "./components/CurrentPlan";
 import Plans from "./components/Plans";
 import styles from "./SubscriptionPage.module.scss";
 
 function SubscriptionPage() {
-    const currentPlan: string = "free";
+    const dispatch = useDispatch();
+    const firebaseService = new FirebaseService();
+    const userData: UserData = useSelector((state: StateModel) => state.user.userData);
 
-    return (
+    useEffect(() => {
+        if (!userData.planId)
+            new Promise(async () => {
+                dispatch(setUserData(await firebaseService.getUserData()));
+            });
+    }, []);
+
+    return userData.planId !== undefined ? (
         <div className={styles["subs-page-wrapper"]}>
-            {currentPlan === "free" ? <Plans currentPlan={currentPlan} /> : <CurrentPlan currentPlan={currentPlan} />}
+            {userData.planId === 0 ? (
+                <Plans />
+            ) : (
+                <CurrentPlan currentPlanId={userData.planId} currentPlanName={userData.planName} />
+            )}
+        </div>
+    ) : (
+        <div className={styles.loading}>
+            <LoadingSvgComponent />
         </div>
     );
 }
--- src/pages/subscription/components/CurrentPlan.tsx
+++ src/pages/subscription/components/CurrentPlan.tsx
@@ -1,23 +1,33 @@
 import PremiumPlanSvgComponent from "../../../../components/Svg/PremiumPlanSvg";
 import StandartPlanSvgComponent from "../../../../components/Svg/StandartPlanSvg";
+import { subscriptionPlans } from "../const/subscriptionPlans";
 
 import styles from "../SubscriptionPage.module.scss";
 
 type Props = {
-    currentPlan: string;
+    currentPlanId: number;
+    currentPlanName: string;
 };
 
 function CurrentPlan(props: Props) {
     return (
         <>
             <p className={styles["current-plan"]}>
-                You currently have a <span>{props.currentPlan}</span> plan.
+                You currently have a <span>{props.currentPlanName}</span> plan.
             </p>
 
+            {props.currentPlanId === 0 ? null : (
+                <ul className={styles["plan-details"]}>
+                    {subscriptionPlans[props.currentPlanId - 1].items.map((item: string, index: number) => {
+                        return <li key={index}>{item}</li>;
+                    })}
+                </ul>
+            )}
+
             <div className={styles["current-plan-card"]}>
-                {props.currentPlan === "premium" ? (
+                {props.currentPlanId === 2 ? (
                     <PremiumPlanSvgComponent />
-                ) : props.currentPlan === "standart" ? (
+                ) : props.currentPlanId === 1 ? (
                     <StandartPlanSvgComponent />
                 ) : null}
             </div>
