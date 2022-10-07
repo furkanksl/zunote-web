06bb30df3b6df5d4a1dfdf794efafe387359d643
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-10-07 20:16:24 +0300

    Update recorder component for limits of voice time and piece

--- src/pages/home/components/Recorder.tsx
+++ src/pages/home/components/Recorder.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import { AudioData, useRecorder } from "voice-recorder-react";
 
 import MicSvgComponent from "../../../../components/Svg/MicSvg";
 import PauseSvgComponent from "../../../../components/Svg/PauseSvg";
 import PlaySvgComponent from "../../../../components/Svg/PlaySvg";
 import StopSvgComponent from "../../../../components/Svg/StopSvg";
+import UserData from "../../../models/UserData.model";
+import VoiceNote from "../../../models/VoiceNote.model";
 import { setIsRecording, setLapTime } from "../../../redux/features/recorder.reducer";
+import { StateModel } from "../../../redux/store/store";
 import UtilityService from "../../../services/utility.service";
 
 type Props = {
@@ -20,6 +24,11 @@ function Recorder(props: Props) {
     const [isActive, setIsActive] = useState(false);
     const [isStarted, setIsStarted] = useState(false);
 
+    const userData: UserData = useSelector((state: StateModel) => state.user.userData);
+    const voiceNotes = useSelector((state: StateModel) =>
+        state.note.notes.filter((note: any) => note instanceof VoiceNote)
+    );
+
     const { time, data, stop, start, pause, resume } = useRecorder();
 
     useEffect(() => {
@@ -32,9 +41,31 @@ function Recorder(props: Props) {
         dispatch(
             setLapTime(utilityService.formatRecorderTime(time?.m) + ":" + utilityService.formatRecorderTime(time?.s))
         );
+
+        if (time.m === 3 && userData.planId === 0) {
+            stopVoiceRecording();
+
+            toast.info("You reached the 3 mins single recording time limit in you Free plan");
+        } else if (time.m === 10 && userData.planId === 1) {
+            stopVoiceRecording();
+
+            toast.info("You reached the 10 mins single recording time limit in you Standart plan");
+        } else if (time.m === 30 && userData.planId === 2) {
+            stopVoiceRecording();
+
+            toast.info("You reached the 30 mins single recording time limit in you Premium plan");
+        }
     }, [time]);
 
     function startVoiceRecording() {
+        if (userData.planId === 0 && voiceNotes?.length >= 10) {
+            toast.info("You reached the 10 voices note limit in you Free plan");
+            return;
+        } else if (userData.planId === 1 && voiceNotes?.length >= 100) {
+            toast.info("You reached the 100 voices note in you Standart plan");
+            return;
+        }
+
         start();
 
         setIsActive(true);
